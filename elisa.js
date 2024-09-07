2bd5534e750167bacae64c4da6fd821d2d200dce
Mario Acendra <mariojoacre31@gmail.com>
2024-09-06 20:02:17 -0500

    Agregads funciones

--- elisa.js
+++ elisa.js
@@ -4,8 +4,14 @@ function suma(a,b){
 
 }
 
-function suma(a,b){
+function resta(a,b){
 
     return a-b;
 
 }
+
+
+num1=parseInt(prompt("Ingrese el numero 1: "))
+num2=parseInt(prompt("Ingrese el numero 2: "))
+
+alert(`La suma es: ${suma(num1,num2)} \n La resta es ${resta(num1,num2)}`)
